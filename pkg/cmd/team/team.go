991c4c02824615f5913c84892581f518cf6f3810
w-haibara <hwhaibarawataru@gmail.com>
2022-08-12 18:26:31 +0900

    fix team command

--- pkg/cmd/team/team.go
+++ pkg/cmd/team/team.go
@@ -33,7 +33,7 @@ func teamRun(opts TeamOptions) error {
 	}
 
 	for _, team := range client.Teams {
-		fmt.Fprintf(iostreams.IO.Out, "%s", team.Name)
+		fmt.Fprintf(iostreams.IO.Out, "%s, %s", team.Name, team.ID)
 	}
 
 	return nil
